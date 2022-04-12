8d514da4db0ec37040f3c22ba138888cdcdd2aa8
Your Name <phamtienquan2001@gmail.com>
2022-04-13 01:27:36 +0700

    done comment
    23

--- utils/db.js
+++ utils/db.js
@@ -3,11 +3,11 @@ import knexObj from "knex";
 const knex = knexObj({
   client: "mysql2",
   connection: {
-    host: "localhost",
+    host: "db4free.net",
     port: 3306,
-    user: "root",
-    password: "123456",
-    database: "qlbh",
+    user: "clothesshopapp",
+    password: "b2ef08c3",
+    database: "clothesshopapp",
   },
   pool: {
     min: 0,
@@ -17,7 +17,13 @@ const knex = knexObj({
 
 export default knex;
 
-
+/*connection: {
+  host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "123456",
+      database: "qlbh",
+},*/
 
 
 /*
